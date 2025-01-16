0aa195accf2b22b65ace6ce64a9690772c9a5aeb
frostbitecloudhost <157946678+frostbitecloudhost@users.noreply.github.com>
2025-01-17 02:50:04 +0530

    Update config.js

--- config.js
+++ config.js
@@ -2,13 +2,13 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const config = {
-	SESSION_ID: process.env.SESSION_ID || '',
+	SESSION_ID: process.env.SESSION_ID || 'XSTRO_94_19_95',
 	SUDO: process.env.SUDO || '',
 	API_ID: process.env.API_ID || 'https://xstro-api-4fb28ece11a9.herokuapp.com',
-	BOT_INFO: process.env.BOT_INFO || 'αѕтяσχ11;χѕтяσ м∂',
-	STICKER_PACK: process.env.STICKER_PACK || 'мα∂є ву;χѕтяσ мυℓтι ∂єνι¢є вσт',
+	BOT_INFO: process.env.BOT_INFO || '☩ 𝐁𝐥𝐮𝐝𝐌𝐝 ☩',
+	STICKER_PACK: process.env.STICKER_PACK || '☩ 𝐁𝐥𝐮𝐝𝐌𝐝 ☩',
 	WARN_COUNT: process.env.WARN_COUNT || 3,
-	TIME_ZONE: process.env.TIME_ZONE || 'Africa/Lagos',
+	TIME_ZONE: process.env.TIME_ZONE || 'Ohio',
 	DEBUG: process.env.DEBUG || false,
 	VERSION: '1.2.3'
 };
